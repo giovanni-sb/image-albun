e3ed0d3530a229c04861522d4cf424fdeb94dae9
giovanni-sb <giovannidossb@gmail.com>
2020-07-07 18:46:09 -0300

    working for and style tweaks

--- main-script.js
+++ main-script.js
@@ -5,6 +5,24 @@ const api_url = 'https://projetofinal-ppw.herokuapp.com/api/'+id
 // var data = requisition.then(function(response){
 //     return response.json()
 // })
+var data_requisition = fetch(api_url)
+var data = data_requisition.then(function(response){
+    return response.json()
+})
+var box = document.getElementById('albun-box')
+data.then(function(data){
+    console.log(data)
+    for(var item of data){
+        console.log(item)
+        var div = document.createElement('div')
+        div.setAttribute('class', 'img-thumbnail')
+        var img = document.createElement('img')
+        img.setAttribute('src', item.location)
+        img.setAttribute('alt', item.description)
+        div.appendChild(img)
+        box.appendChild(div)
+    }
+})
 
 var albuns = document.getElementsByClassName("albun-cover")
 for(var i = 0; i<albuns.length; i++){
@@ -20,7 +38,8 @@ form.addEventListener('submit', function(e){
     var url = document.getElementById('url-input')
     var alt = document.getElementById('alt-input')
     var extension = url.value.split(".").slice(-1)[0]
-    if(extension == 'jpg' || extension == 'png' || extension == 'bmp'){
+    console.log(extension)
+    if(extension == 'jpg' || extension == 'png' || extension == 'bmp'|| extension == 'svg'){
         console.log('imagem')
         add_to_api(url.value, alt.value)
     }
@@ -40,5 +59,21 @@ function add_to_api(url, alt){
     }
 
     var requisition = fetch(api_url, options)
-    window.location.reload(true)
+    var data = requisition.then(function(response){
+        console.log(response.status)
+        return response.json()
+    }).finally(function(){window.location.reload(true)})
+    data.then(function(){
+        console.log(data)
+    })
 }
+
+document.getElementsByTagName('h1')[0].addEventListener('click', function(){
+    var options = {
+        method: 'DELETE',
+    }
+    var requisition = fetch(api_url, options)
+    requisition.then(function(response){
+        console.log(response.status)
+    })
+})
